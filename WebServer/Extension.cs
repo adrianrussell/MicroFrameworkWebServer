036f38813db932f764990068b8ac5ef961c85109
AdrianRussell <adrianrussell@iinet.net.au>
2011-05-21 10:29:41 +1000

    Initial commit of web server from netduino forums users. http://forums.netduino.com/index.php?/user/276-fred/ http://forums.netduino.com/index.php?/user/209-hari/ http://forums.netduino.com/index.php?/user/111-omar-oz/

---
+++ WebServer/Extension.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.SPOT;
+
+// This is needed for extension methods to work
+namespace System.Runtime.CompilerServices
+{
+    public class ExtensionAttribute : Attribute { }
+}
+
+namespace NetduinoPlusWebServer
+{
+    /// <summary>
+    /// Extension methods
+    /// </summary>
+    public static class Extension
+    {
+        /// <summary>
+        /// Replace characters in a string
+        /// </summary>
+        /// <param name="stringToSearch"></param>
+        /// <param name="charToFind"></param>
+        /// <param name="charToSubstitute"></param>
+        /// <returns></returns>
+        public static string Replace(this string stringToSearch, char charToFind, char charToSubstitute)
+        {
+            // Surely there must be nicer way than this?
+            char[] chars = stringToSearch.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+                if (chars[i] == charToFind) chars[i] = charToSubstitute;
+
+            return new string(chars);
+        }
+
+    }
+}
---
+++ WebServer/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using System.IO;
+
+namespace NetduinoPlusWebServer
+{
+    public class Program
+    {
+        const string WebFolder = "\\SD\\Web";
+
+        public static void Main()
+        {
+
+            Listener webServer = new Listener(RequestReceived);
+
+            OutputPort led = new OutputPort(Pins.ONBOARD_LED, false);
+            while (true)
+            {
+                // Blink LED to show we're still responsive
+                led.Write(!led.Read());
+                Thread.Sleep(500);
+            }
+
+        }
+
+
+        private static void RequestReceived(Request request)
+        {
+            // Use this for a really basic check that it's working
+            //request.SendResponse("<html><body><p>Request from " + request.Client.ToString() + " received at " + DateTime.Now.ToString() + "</p><p>Method: " + request.Method + "<br />URL: " + request.URL +"</p></body></html>");
+
+            // Send a file
+            TrySendFile(request);
+
+        }
+
+        /// <summary>
+        /// Look for a file on the SD card and send it back if it exists
+        /// </summary>
+        /// <param name="request"></param>
+        private static void TrySendFile(Request request)
+        {
+            // Replace / with \
+            string filePath = WebFolder + request.URL.Replace('/', '\\');
+
+            if (File.Exists(filePath))
+                request.SendFile(filePath);
+            else
+                request.Send404();
+        }
+
+    }
+}
