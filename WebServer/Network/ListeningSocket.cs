9847a625e1df0c74536eca7c74c84ab8a61641f5
AdrianRussell <adrianrussell@iinet.net.au>
2011-06-10 10:33:19 +1000

    Variable renaming

--- WebServer/Network/ListeningSocket.cs
+++ WebServer/Network/ListeningSocket.cs
@@ -1,28 +1,31 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace Server.Network
 {
     public class ListeningSocket : IListeningSocket
     {
-        private readonly Socket _listeningSocket;
+        private readonly Socket _socket;
         private readonly int _portNumber;
 
         public ListeningSocket(int portNumber) {
             _portNumber = portNumber;
 
-            _listeningSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            _listeningSocket.Bind(new IPEndPoint(IPAddress.Any, _portNumber));
-            _listeningSocket.Listen(10);
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            _socket.Bind(new IPEndPoint(IPAddress.Parse("192.168.0.16"), _portNumber));
+            _socket.Listen(10);
+            Thread.Sleep(50);
         }
 
 
         public Socket Accept() {
-            return _listeningSocket.Accept();
+            return
+                _socket.Accept();
         }
 
         public void Dispose() {
-            if (_listeningSocket != null) _listeningSocket.Close();
+            if (_socket != null) _socket.Close();
         }
     }
 }
